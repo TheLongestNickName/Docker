9431dc0df60829d1af770e74af419ee5941ed1df
Sergey Knysh <apc71405@gmail.com>
2021-01-18 08:26:02 +0200

    easy colculator

---
+++ Doc/homeWork8/index.js
@@ -0,0 +1,27 @@
+function sum(a, b){
+    return a + b;
+}
+function sub(a, b) {
+    return a + b;
+}
+function div(a, b){
+    return a / b;
+}
+function  mult(a, b){
+    return a * b
+}
+
+function calculator(operator, operant1, operant2){
+    if (operator == "sum") {
+        return sum(operant1, operant2);
+    }else if (operator == "sub") {
+        return sub(operant1, operant2 );
+    } else  if (operator == "div") {
+        return div(operant1,operant2);
+    }  else if  (operator == "mult"){
+        return mult(operant1,operant2);
+    }
+
+}
+
+
