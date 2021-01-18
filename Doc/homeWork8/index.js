745b9d424a89c91d59867b132e8516531938a9d7
Sergey Knysh <apc71405@gmail.com>
2021-01-18 10:50:43 +0200

    create calculator on two actions

--- Doc/homeWork8/index.js
+++ Doc/homeWork8/index.js
@@ -1,27 +1,63 @@
 function sum(a, b){
     return a + b;
-};
+}
 function sub(a, b) {
-    return a + b;
-};
+    return a - b;
+}
 function div(a, b){
     return a / b;
-};
+}
 function  mult(a, b){
     return a * b
-};
+}
 
-function calculator(operator, operant1, operant2){
-    if (operator == "sum") {
-        return sum(operant1, operant2);
-    }else if (operator == "sub") {
-        return sub(operant1, operant2 );
-    } else  if (operator == "div") {
-        return div(operant1,operant2);
-    }  else if  (operator == "mult"){
-        return mult(operant1,operant2);
+function calculator(operator, operant1, operant2, operator2, operant3){
+
+    let resault1;
+
+    if (operator == "sum" && !operator2) {
+        resault1 = sum(operant1, operant2);
+    } else if (operator == "sub" && !operator2){
+        resault1 = sub(operant1, operant2);
+    } else if (operator == "div" && !operator2) {
+        resault1 = div(operant1, operant2);
+    } else if (operator == "mult" && !operator2) {
+        resault1 = mult(operant1, operant2);
+    } else if (operator == "sum" && operator2 == "sum" ) {
+        resault1 = sum(sum(operant1, operant2), operant3);
+    } else if (operator == "sum" && operator2 == "sub" ){
+        resault1 = sub(sum(operant1, operant2), operant3);
+    } else if (operator == "sum" && operator2 == "div"){
+        resault1 = sum(operant1, div(operant2,operant3));
+    } else if (operator == "sum" && operator2 == "mult" ) {
+        resault1 = sum(operant1, mult(operant2, operant3))
+    }else if (operator == "sub" && operator2 == "sum" ) {
+        resault1 = sub(sum(operant1, operant2), operant3);
+    } else if (operator == "sub" && operator2 == "sub" ){
+        resault1 = sub(sub(operant1, operant2), operant3);
+    } else if (operator == "sub" && operator2 == "div"){
+        resault1 = sub(operant1, div(operant2,operant3));
+    } else if (operator == "sub" && operator2 == "mult" ) {
+        resault1 = sub(operant1, mult(operant2, operant3))
+    }else if (operator == "div" && operator2 == "sum" ) {
+        resault1 = sum(div(operant1, operant2), operant3);
+    } else if (operator == "div" && operator2 == "sub" ){
+        resault1 = sub(div(operant1, operant2), operant3);
+    } else if (operator == "div" && operator2 == "div"){
+        resault1 = div(operant1, div(operant2,operant3));
+    } else if (operator == "div" && operator2 == "mult" ) {
+        resault1 = div(operant1, mult(operant2, operant3));
+    }else if (operator == "mult" && operator2 == "sum" ) {
+        resault1 = sum(mult(operant1, operant2), operant3);
+    } else if (operator == "mult" && operator2 == "sub" ){
+        resault1 = sub(mult(operant1, operant2), operant3);
+    } else if (operator == "mult" && operator2 == "div"){
+        resault1 = mult(operant1, div(operant2,operant3));
+    } else if (operator == "mult" && operator2 == "mult" ) {
+        resault1 = mult(operant1, mult(operant2, operant3));
     }
 
+return resault1
 }
 
 let globalObj = {
@@ -30,7 +66,7 @@ let globalObj = {
 
 function set(name, value){
     globalObj[name] = value;
-};
+}
 
 function get(name){
     if(globalObj.hasOwnProperty(name)){
